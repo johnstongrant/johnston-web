3627c490c0e067aa32e0438ac0883957b25ea907
Grant Johnston <grantjohnston02@gmail.com>
2024-05-24 01:39:39 -0500

    favi

--- website/src/components/ProjectDetails.jsx
+++ website/src/components/ProjectDetails.jsx
@@ -1,5 +1,12 @@
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
   export default function ProjectDetails({title, about, link, images}) {
+    const {pathname} = useLocation()
+    useEffect(() => {
+      window.scrollTo(0,0)
+    }, [pathname])
+  
 
     return link !== "" ?
  (
